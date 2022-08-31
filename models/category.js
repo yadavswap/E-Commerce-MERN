1aeecb33d91f9256d45ac87db8fc72461ee096e3
Swapnil Santosh Yadav <swapnilyadav@technoscore.net>
2022-08-31 13:39:00 +0530

    created category  schema

---
+++ models/category.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 32,
+        trim: true
+    },
+}, {
+    timestamp: true
+});
+module.exports = mongoose.model("Category", categorySchema);
