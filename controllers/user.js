5703d2daf03783c89ba4d850140d3ade640cd1a9
Swapnil Santosh Yadav <swapnilyadav@technoscore.net>
2022-09-09 14:55:56 +0530

    update user info

--- controllers/user.js
+++ controllers/user.js
@@ -35,3 +35,26 @@ exports.getAllUsers = (req,res)=>{
         return res.json(users);
     })
 }
+
+// update user
+
+exports.getUpdateUser = (req,res)=>{
+    User.findByIdAndUpdate(
+        {_id:req.profile._id},
+        {$set:req.body},
+        {new:true,useFindAndModify:false},
+        (err,user)=>{
+            if(err){
+                return res.status(400).json({
+                    error:"You are not authorized to update this user"
+                });
+            }
+            user.salt = undefined;
+            user.encry_password = undefined;
+            user.encry_password = undefined;
+            user.createdAt = undefined;
+            user.updatedAt = undefined;
+            return res.json(user);
+        }
+    )
+}
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const {getUserById,getUser,getAllUsers} = require("../controllers/user");
+const {getUserById,getUser,getAllUsers,getUpdateUser} = require("../controllers/user");
 const {isSignedIn,isAuthenticated,isAdmin } = require("../controllers/auth");
 
 
 router.param("userId",getUserById);
 router.get("/user/:userId",isSignedIn,isAuthenticated,getUser);
 router.get("/users",getAllUsers);
+router.put("/user/update/:userId",isSignedIn,isAuthenticated,getUpdateUser);
 
 module.exports = router;
